b8295800e79d642527557f0a38d94095dee97630
Jonathan <jonathan.george.dev@gmail.com>
2020-09-08 03:51:55 -0700

    Updates

---
+++ precache-manifest.e97db7003fa6f5f53fcf621dc61b1459.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ff3b985a4fb3375c612f8a1aaac24e1",
+    "url": "/price_toggle/index.html"
+  },
+  {
+    "revision": "761f5108b906b7ff08a9",
+    "url": "/price_toggle/static/css/main.1cb71b88.chunk.css"
+  },
+  {
+    "revision": "bf6d1e5f41619bb5a80e",
+    "url": "/price_toggle/static/js/2.165b2a4d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/price_toggle/static/js/2.165b2a4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "761f5108b906b7ff08a9",
+    "url": "/price_toggle/static/js/main.73634c40.chunk.js"
+  },
+  {
+    "revision": "aceaf397e29679816d7b",
+    "url": "/price_toggle/static/js/runtime-main.b7332e17.js"
+  }
+]);
